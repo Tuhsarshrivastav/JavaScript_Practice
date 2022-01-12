a159ea5ae17ea8a3bc79d3554657a2d68332a768
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-12 10:41:56 +0530

    call example

---
+++ Call, apply Bind.js
@@ -0,0 +1,19 @@
+// Call, apply & Bind
+
+let userDetails = {
+  name: "Tushar shrivastav",
+  age: 23,
+  Designation: "Full Stack Engineer",
+};
+
+let printDetails = function () {
+  console.log(this);
+};
+printDetails.call(userDetails);
+
+let userDetails1 = {
+  name: "sushar",
+  age: 23,
+  Designation: "backend Engineer",
+};
+printDetails.call(userDetails1);
