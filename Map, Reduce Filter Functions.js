5a868599a6b38f8bf7a3ba2c7d6234208071f395
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-12 11:03:25 +0530

    foreach example

---
+++ Map, Reduce Filter Functions.js
@@ -0,0 +1,14 @@
+const companies = [
+  { name: "Google", category: "Product Based", start: 1981, end: 2004 },
+  { name: "Amazon", category: "Product Based", start: 1992, end: 1992 },
+  { name: "Pytem", category: "Product Based", start: 1999, end: 2007 },
+  { name: "Coforge", category: "Service Based", start: 1989, end: 2010 },
+  { name: "Mindtree", category: "Service Based", start: 1989, end: 2010 },
+];
+
+const ages = [33, 22, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// foreach
+companies.forEach((companie, index) => {
+  console.log(companie, index);
+});
