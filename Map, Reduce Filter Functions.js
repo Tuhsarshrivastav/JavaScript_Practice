ef1f2443650440bc28e3c8f265a2f351ef04c5f5
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-12 11:18:58 +0530

    filter 2nd example

--- Map, Reduce Filter Functions.js
+++ Map, Reduce Filter Functions.js
@@ -14,3 +14,7 @@ const ages = [33, 22, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // filter
 // const finalage = ages.filter(age => age > 20);
 // console.log(finalage);
+
+// filter 2nd exampe
+const sbc = companies.filter((compney) => compney.category == "Service Based");
+console.log(sbc);
